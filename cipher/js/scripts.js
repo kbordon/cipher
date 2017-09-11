b60a5fd89fe14e69eeb2426f3dd584c02d613f40
Kimberly Bordon and Valia Dempsey <valia@>
2017-09-11 15:34:28 -0700

    Add functions to capitalize letters.

--- cipher/js/scripts.js
+++ cipher/js/scripts.js
@@ -3,6 +3,12 @@ alert(sentence);
 
 var sentenceLength = sentence.length;
 
+var firstLetter = sentence.charAt(0);
+var capFirstLetter = firstLetter.toUpperCase();
+
+var newStart = sentence.replace(firstLetter, capFirstLetter);
+alert(newStart);
+
 var lastLetter = sentence.charAt(sentenceLength - 1);
 alert(lastLetter);
 
@@ -11,3 +17,12 @@ alert(capLastLetter);
 
 var newSentence = sentence.replace(lastLetter, capLastLetter);
 alert(newSentence);
+
+//var example = RegExp(lastLetter, "$&");
+//var newSentence = sentence.replace(example, capLastLetter);
+//alert(newSentence);
+
+alert(capFirstLetter + capLastLetter); //this will capitalize first and last letter
+
+var revLetters = (capLastLetter + capFirstLetter);
+alert(revLetters);
